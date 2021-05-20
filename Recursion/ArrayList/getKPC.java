d0cb309be0f2dd23a7488fb23b870d732745f334
Anany-Gupta <ananygupta@gmail.com>
2021-05-20 19:55:28 +0530

    Improved KPC code

--- Recursion/ArrayList/getKPC.java
+++ Recursion/ArrayList/getKPC.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class getKPC {
-
+    static String keypad[]={".;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
@@ -20,76 +20,12 @@ public class getKPC {
         String subS = str.substring(1);
         ArrayList<String> prevResult = KPC(subS);//1
         ArrayList<String> myResult = new ArrayList<>();
-        char num = str.charAt(0);
-        ArrayList<String> presentChars = new ArrayList<>();
-        switch (num) {
-            // 0 -> .;
-            // 1 -> abc
-            // 2 -> def
-            // 3 -> ghi
-            // 4 -> jkl
-            // 5 -> mno
-            // 6 -> pqrs
-            // 7 -> tu
-            // 8 -> vwx
-            // 9 -> yz
-            case '0':
-                presentChars.add(".");
-                presentChars.add(";");
-                break;
-            case '1':
-                presentChars.add("a");
-                presentChars.add("b");
-                presentChars.add("c");
-                break;
-            case '2':
-                presentChars.add("d");
-                presentChars.add("e");
-                presentChars.add("f");
-                break;
-            case '3':
-
-                presentChars.add("g");
-                presentChars.add("h");
-                presentChars.add("i");
-                break;
-            case '4':
-                presentChars.add("j");
-                presentChars.add("k");
-                presentChars.add("l");
-                break;
-            case '5':
-                presentChars.add("m");
-                presentChars.add("n");
-                presentChars.add("o");
-                break;
-            case '6':
-
-                presentChars.add("p");
-                presentChars.add("q");
-                presentChars.add("r");
-                presentChars.add("s");
-                break;
-
-            case '7':
-                presentChars.add("t");
-                presentChars.add("u");
-                break;
-
-            case '8':
-                presentChars.add("v");
-                presentChars.add("w");
-                presentChars.add("x");
-                break;
-            case '9':
-                presentChars.add("y");
-                presentChars.add("z");
-                break;
-
-        }
-        for (String pAns : presentChars) {
+        int num = Integer.parseInt(str.charAt(0)+"");
+    
+        
+        for (int i=0;i<keypad[num].length();i++) {
             for (String prev : prevResult) {
-                myResult.add(pAns + prev);
+                myResult.add(keypad[num].charAt(i)+ prev);
             }
         }
         return myResult;
